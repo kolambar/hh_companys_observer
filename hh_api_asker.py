932591c7f291b297eea9729e13b87eb952035e2c
Nikolai <kolamba.r@gmail.com>
2023-07-25 19:47:23 +0300

    func get_vacancy

---
+++ hh_api_asker.py
@@ -0,0 +1,26 @@
+import requests
+
+
+class HhApiAsker:
+    """
+    Подключается к HeadHunter
+    """
+
+    @staticmethod
+    def get_vacancy(employer):
+        """
+        Подключается к HeadHunter и получает вакансии нужных работодателей
+        """
+        params = {
+            "per_page=": 100,
+            "page": 1,
+            "employer_id": employer
+        }
+        return requests.get('https://api.hh.ru/vacancies/', params=params).json()
+
+
+vac = HhApiAsker.get_vacancy('1455')
+print(vac)
+
+#  employer_id = {Яндекс: 1740, hh: 1455, Сбер: 3529, точка: 2324020, инфотекст: 3778, icl: 32570, HR Prime: 4759060,
+#  Сима: 238161
